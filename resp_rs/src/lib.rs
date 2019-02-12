e7de5d45431c1d86636fb5bb82d497f3f5736c42
Abhijit Gadgil <gabhijit@iitbombay.org>
2019-02-13 00:15:54 +0530

    Little more

--- resp_rs/src/lib.rs
+++ resp_rs/src/lib.rs
@@ -15,18 +15,26 @@ impl Value {
 
     pub fn from_resp(input: &str) -> Result<Value, std::io::Error> {
 
+        let mut last =  Value::Null;
+
         for item in input.split("\r\n") {
-            let last = Value::parse_item(item);
-            match last {
-                Err(_) => println!("None"),
-                Ok(x) => println!("{:?}", x),
+            let result = Value::parse_item(item);
+            match result {
+                Err(err) =>  {
+                    println!("None");
+                    return Err(err)
+                },
+                Ok(x) => {
+                    println!("{:?}", x);
+                    last = x
+                },
             }
         }
-        Ok(Value::Null)
+        Ok(last)
     }
 
     fn parse_item(item: &str) -> Result<Value, std::io::Error> {
-        match  item {
+        match item {
             "$-1" => Ok(Value::Null),
             "*-1" => Ok(Value::NullArray),
             _  => Ok(Value::String(String::from(item))),
