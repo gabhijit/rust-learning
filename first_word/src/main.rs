0be76cea0d842282971b9972c8e0f3b731e09e32
Abhijit Gadgil <gabhijit@iitbombay.org>
2020-05-14 22:09:15 +0530

    Added comments about mutable / immutable references

--- first_word/src/main.rs
+++ first_word/src/main.rs
@@ -1,17 +1,33 @@
 fn main() {
-    println!("Hello, world!");
 
-    let mut s = String::from("hello, world!");
+    let mut s = String::from("hello");
+
+    // The reference is an immutable reference
+    // A reference to an immutable string does not simply make
+    // the reference immutable, you've to explicitly make that
+    // reference immutable.
+    change(&mut s);
 
     let word = first_word(&s);
 
     println!("First word is {}!", word);
 
+    // This is allowed 'after' word because word is no longer used
+    // So mutable reference is just fine.
     s.clear();
 
+    // Following 's' is a totally new variable and just works
+    // This has got nothing to do with former 's'. (shadowed)
     let s = "Welcome Again!";
 
     println!("First word of {} is {}.", s, first_word(s));
+
+    // This is another 's'
+    let s = String::from("Hello World!");
+
+    // A mutable reference to immutable string literal,
+    // of course won't work. Try uncommenting line below and compiling
+    // change(&mut s);
 }
 
 // Changing the input parameter to `&str` would make our API more
@@ -31,5 +47,11 @@ fn first_word(s: &str) -> &str {
     }
 
     &s[..]
+
+}
+
+fn change(s: &mut String) {
+
+    s.push_str(", World!");
 
 }
