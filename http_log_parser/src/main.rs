f0cdeb05f089ec6993a3e41425eb2625bf8e808a
Abhijit Gadgil <gabhijit@iitbombay.org>
2020-05-17 13:30:55 +0530

    First level parsing done of HTTP Log
    1. Now we get a vector of HTTP Log Entries
    2. Parsing this is separated into a separate function
    
    x

--- http_log_parser/src/main.rs
+++ http_log_parser/src/main.rs
@@ -7,9 +7,79 @@ use std::path;
 
 // BufRead is required because we are using the `lines` method
 // on the BuffReader. If we don't use it, we cannot use the `lines` method
-// compiler croaks.
+// compiler croaks. Also this {self} for std::io:: means we can use
+// std::io (the top level module)
 use std::io::{self, BufRead};
 use std::fs;
+
+use std::vec::IntoIter;
+
+// Deriving debug trait for {:?} printing
+#[derive(Debug)]
+struct HTTPLogEntry {
+    ipaddress: String,
+    url: String,
+    status_code: u16,
+    transferred: u32
+}
+
+// In the following function we could return a Vector of &HTTPLogEntry,
+// But the problem with that is, `entry` that we push into the vector is
+// scoped only to the function (local variable), so passing the reference
+// to that and returning that won't be possible. We can indeed do that if
+// we allocate entries on Heap and then passed their reference. That can
+// be done by implementing `new` for the HTTPLogEntry. We've not done that,
+// but we can try doing that.
+//
+// Optionally we can pass 'consuming' Vec<HTTPLogEntry> where when we 'push'
+// the entry, a copy is made and it is passed (so something like `new` above
+// is done for us by the compiler.
+//
+// In general though, we'd typically, iterate over such entries, so it's perhaps
+// a good idea to make this into an iterator and pass that, which is what
+// we are using below.
+//
+// Also a note about this whole AsRef<path::Path>, Basically this means, this
+// function will work on anything that will take a Path as an argument. A normal
+// String, &str (A string slice) can be converted into a Path and hence just work
+// as easy.
+//
+// We are returning io::Result for the same reasons, we want to use the short-cut
+// '?' operator on most I/O.
+fn parse_weblog_file<P: AsRef<path::Path>>(filename: P) -> io::Result<IntoIter<HTTPLogEntry>> {
+
+    let mut http_entries = Vec::new();
+
+    let f = fs::File::open(filename)?;
+    println!("Opened file is {:?}", f);
+
+    let reader = io::BufReader::new(f);
+    println!("Bufreader: {:?}", reader);
+
+    // line below is an io::Result<String>, So we'll have to
+    // extract the ok part and then unwrap it. Is there a cleaner way
+    // to do it?
+    for line in reader.lines() {
+        let line = line.ok().unwrap();
+        let tokens: Vec<&str> = line.split(' ').collect();
+        let ipaddress = String::from(tokens[0]);
+        let url = String::from(tokens[6]);
+        let status_code = tokens[8];
+        let transferred = tokens[9];
+
+        let entry = HTTPLogEntry {
+            ipaddress: ipaddress,
+            url: url,
+            status_code: status_code.parse().unwrap(),
+            transferred: transferred.parse().unwrap_or(0)
+        };
+
+        http_entries.push(entry);
+
+    }
+
+    Ok(http_entries.into_iter())
+}
 
 
 // We have made the return type of this function as io:Result<()>
@@ -23,7 +93,6 @@ use std::fs;
 // io::Result<T> is actually a shortcut for
 // std::result::Result<T , io:Error>
 //
-
 fn main() -> io::Result<()> {
     let filename = std::env::args()
         .nth(1)
@@ -37,17 +106,10 @@ fn main() -> io::Result<()> {
         println!("Path does not exist.");
     }
 
-    let mut f = fs::File::open(filename)?;
-    println!("Opened file is {:?}", f);
+    let entries = parse_weblog_file(filename)?;
 
-    let reader = io::BufReader::new(f);
-    println!("Bufreader: {:?}", reader);
-
-    // line below is an io::Result<String>, So we'll have to
-    // extract the ok part and then unwrap it. Is there a cleaner way
-    // to do it?
-    for line in reader.lines() {
-        println!("{}", line.ok().unwrap());
+    for entry in entries {
+        println!("{:?} ", entry);
     }
 
     // Below will panic as unwrap is not allowed on None value
