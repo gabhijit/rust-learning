e6182d7ada7afec13807846282f69d2cee654ade
Abhijit Gadgil <gabhijit@iitbombay.org>
2020-06-03 16:59:54 +0530

    First cut implementation: Arrays and Strings
    Completed parsing arrays and strings. First cut implementation.
    Started with a `RedisProtocolParser` struct and implemented the parsing
    methods inside `impl` block of that. That looks more correct. Previously
    we were using the parsing methods as `scoped` methods in the `impl`
    block of the `Value`. That was not so semantically correct.

--- resp_rs/src/lib.rs
+++ resp_rs/src/lib.rs
@@ -4,6 +4,123 @@
 use std::error::Error;
 use std::fmt;
 
+
+#[derive(Debug)]
+pub struct RedisProtocolParser<'a> {
+    input: &'a str,
+    index: usize
+}
+
+impl <'a> RedisProtocolParser<'a> {
+
+    pub fn new(input: &'a str) -> Self {
+        Self {
+            input,
+            index: 0
+        }
+    }
+
+    pub fn advance(&mut self,howmuch: usize) -> () {
+        self.index += howmuch;
+    }
+
+    pub fn consumed(&self) -> bool {
+        self.index >= self.input.len()
+    }
+
+    pub fn parse(&mut self) -> Result<(), ValueError> {
+
+        while !self.consumed() {
+            let value = self.parse_one_value()?;
+            println!("Parsed {:?}", value);
+        }
+
+        Ok(())
+    }
+
+    fn parse_one_value(&mut self) -> Result<Value, ValueError> {
+
+        println!("Consumed: {:?}", self.consumed());
+
+        let text_str = &self.input[self.index..];
+        let first = text_str.as_bytes().get(0);
+        match first {
+            Some(b'*') => { self.parse_array() },
+            Some(b'$') => { self.parse_string() },
+                _ => { println!("First: {:?}", first);
+                    Err(ValueError::new("Not Supported!")) }
+        }
+    }
+
+    fn parse_array(&mut self) -> Result<Value, ValueError> {
+
+        let text_str = &self.input[self.index..];
+
+        if let Some(idx) = text_str.find("\r\n") {
+
+           println!("index1: {}", self.index);
+           self.advance(idx+2);
+           println!("index2: {}", self.index);
+
+            let length = text_str[1..idx].parse::<i32>()?;
+            if length > 0 {
+                let mut arr = Vec::new();
+                for _ in 0..length {
+                    if let Ok(value) = self.parse_one_value() {
+                        arr.push(value);
+                        println!("arr: {:?}, index: {:?}", arr, self.index);
+                    } else {
+                        println!("arr: {:?}, idx: {:?}", arr, idx);
+                        return Err(ValueError::new("Unable to parse value"));
+                    }
+                }
+                Ok(Value::Array(arr))
+            } else {
+                Ok(Value::NullArray)
+            }
+        } else {
+            Err(ValueError::new("Unable to parse!"))
+        }
+    }
+
+    fn parse_string(&mut self) -> Result<Value, ValueError> {
+
+        let text_str = &self.input[self.index..];
+
+        if let Some(idx) = text_str.find("\r\n") {
+
+
+            println!("index3: {}", self.index);
+            self.advance(idx+2);
+            println!("index4: {}", self.index);
+
+            let length = text_str[1..idx].parse::<i32>()?;
+
+            println!("{:?}", length);
+
+            if length > 0 {
+                let strval = String::from(&text_str[idx + 2 .. idx + 2 + length as usize]);
+
+                println!("index5: {}", self.index);
+                self.advance(length as usize + 2);
+                println!("index6: {}", self.index);
+
+                return Ok(Value::String(strval));
+            } else {
+
+                println!("index7: {}", self.index);
+                self.advance(2);
+                println!("index8: {}", self.index);
+                return Ok(Value::NullString);
+            }
+        }
+
+        Err(ValueError::new("Unable to Parse"))
+
+    }
+}
+
+
 #[derive(Debug)]
 pub enum Value {
     Null,
@@ -19,97 +136,27 @@ pub struct ValueError {
     cause: String
 }
 
+// We need to define our own Error Type that can be used
+// as a return type.
 impl ValueError {
     fn new(cause: &str) -> ValueError {
         ValueError { cause: String::from(cause) }
     }
 }
 
+// This is required to be defined for our 'Error' Types
 impl fmt::Display for ValueError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}", self.cause)
     }
 }
 
+// Implements our `ValueError` as an Error
 impl Error for ValueError {}
 
+// This allows us to use `?` Operator. The basic idea is as follows
 impl From<std::num::ParseIntError> for ValueError {
     fn from(_: std::num::ParseIntError) -> ValueError {
         ValueError { cause: String::from("Unable to parse") }
-    }
-}
-
-impl Value {
-    pub fn from_resp(input: &str) -> Result<Value, ValueError> {
-
-        let first = input.as_bytes().get(0);
-        match first {
-            Some(b'*') => { Value::parse_list(input) },
-            Some(b'$') => { Value::parse_string(input) },
-                _ => { Err(ValueError::new("Not Supported!")) }
-        }
-            /*
-            if let Some(idx) = input[current..].find("\r\n") {
-                println!("{:?}, {:?}, {:?}", idx, current, input.len());
-                if let Ok(x) = Value::parse_item(&input[current..current + idx]) {
-                    current = current + idx + 2;
-                    if current > input.len() {
-                        break;
-                    }
-                }
-            } else {
-                break;
-            }
-            */
-    }
-
-    fn parse_list(input: &str) -> Result<Value, ValueError> {
-
-        if let Some(idx) = input.find("\r\n") {
-            let length = input[1..idx].parse::<i32>()?;
-            println!("{:?}", length);
-            let mut arr = Vec::new();
-            for _ in 0..length {
-                if let Ok(value) = Value::from_resp(&input[idx + 2..]) {
-                    arr.push(value);
-                } else {
-                    return Err(ValueError::new("Unable to parse value"));
-                }
-            }
-            Ok(Value::Array(arr))
-        } else {
-            Err(ValueError::new("Unable to parse!"))
-        }
-    }
-
-    fn parse_string(input: &str) -> Result<Value, ValueError> {
-
-        if let Some(idx) = input.find("\r\n") {
-            let length = input[1..idx].parse::<i32>()?;
-            println!("{:?}", length);
-            if length > 0 {
-                let strval = String::from(&input[idx + 2 .. idx + 2 + length as usize]);
-                return Ok(Value::String(strval));
-            } else {
-                return Ok(Value::NullString);
-            }
-        }
-
-        Err(ValueError::new("Unable to Parse"))
-
-    }
-
-    fn parse_item(item: &str) -> Result<Value, std::io::Error> {
-        let first = item.as_bytes().get(0);
-        match first {
-            Some(b'*') => println!("*"),
-            Some(b'$') => println!("$"),
-            Some(b':') => println!(":"),
-            Some(b'+') => println!("+"),
-            Some(b'-') => println!("-"),
-            None => println!("None"),
-            _ => println!("Nothing"),
-        }
-        Ok(Value::Null)
     }
 }
--- resp_rs/src/main.rs
+++ resp_rs/src/main.rs
@@ -2,11 +2,14 @@
 use resp_rs;
 
 fn main() {
-    let result = resp_rs::Value::from_resp("$5\r\nhello\r\n").unwrap();
+
+    let mut parser = resp_rs::RedisProtocolParser::new("*3\r\n*-1\r\n$5\r\nhello\r\n$-1\r\n");
+
+    let result = parser.parse().unwrap();
     println!("{:?}", result);
-    let result = resp_rs::Value::from_resp("$-1\r\n").unwrap();
-    println!("{:?}", result);
-    println!("Done!");
+    //let result = resp_rs::Value::from_resp("$-1\r\n").unwrap();
+    //println!("{:?}", result);
+    //println!("Done!");
     /* match result {
         Ok(result) => result,
         Err(Error) => Err(Error),
