7c4339216d5e286dc2217d0cba6f44638347faf8
Abhijit Gadgil <gabhijit@iitbombay.org>
2021-05-26 06:56:52 +0530

    IPv6 Formatter Handling All cases
    1. Fixed issue with a single run of 0 segment
    2. Fixed issue with additional `:` if multiple zero segments are
    followed by non-zero segments

--- ipv6_formatter/src/main.rs
+++ ipv6_formatter/src/main.rs
@@ -1,10 +1,10 @@
-use std::convert::TryInto as _;
-use std::fmt;
-use std::fmt::Write as StdWrite;
+use core::fmt;
+use core::fmt::Write as CoreWrite;
+
+use std::net::Ipv6Addr;
 
 #[derive(Debug)]
 struct ZeroGroup {
-    start: u8,
     count: u8,
 }
 
@@ -17,38 +17,50 @@ enum IPv6Segment {
 #[derive(Default)]
 struct IPv6 {
     segments: Vec<IPv6Segment>,
-    zero_segments: u8,
 }
 
-impl std::fmt::Debug for IPv6 {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> fmt::Result {
+impl core::fmt::Debug for IPv6 {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> fmt::Result {
+        //let mut segments: Vec<String> = Vec::new();
         let mut w = String::new();
-        let mut zero_segment_count = 0;
-        let mut zero_segment = false;
+        let mut long_zero_segments = 0;
+        let mut zero_streak = false;
         for entry in self.segments.iter() {
             match *entry {
                 IPv6Segment::NonZeroSegment(ref num) => {
-                    zero_segment = false;
-                    write!(&mut w, "{:x}", num)
+                    zero_streak = false;
+                    //segments.push(format!("{:x}", num).to_string());
+                    write!(&mut w, "{:x}", num)?;
                 }
                 IPv6Segment::ZeroSegment(ref z) => {
-                    if zero_segment_count == 0 {
-                        write!(&mut w, ":")?;
+                    if z.count == 1 {
+                        let _ = write!(&mut w, "0");
                     } else {
-                        for _ in 0..z.count {
-                            write!(&mut w, "0")?;
+                        long_zero_segments += 1;
+                        if long_zero_segments == 1 {
+                            zero_streak = true;
+                            let _ = w.pop();
+                            write!(&mut w, ":")?;
+                        } else {
+                            for _ in 0..z.count {
+                                //segments.push("0".to_string());
+                                write!(&mut w, "0:")?;
+                            }
+                            let _ = w.pop();
                         }
                     }
-                    zero_segment_count += 1;
-                    zero_segment = true;
-                    write!(&mut w, "")
                 }
             };
-            write!(&mut w, ":x");
+            //segments.push(":".to_string())
+            write!(&mut w, ":")?;
         }
 
-        if !(zero_segment && zero_segment_count == 1) {
+        if !(long_zero_segments == 1 && zero_streak) {
             let _ = w.pop();
+            //segments.push("".to_string());
+            //if allzero_segment {
+            //segments.push("".to_string());
+            //}
         }
 
         write!(f, "{:?}", w)
@@ -58,34 +70,26 @@ impl std::fmt::Debug for IPv6 {
 fn main() {
     let mut ip6addr: IPv6 = IPv6::default();
 
-    // 2001:1234:0000:0000:1b12:0000:0000:1a13
-    let ipv6: [u16; 8] = [
-        0x2001u16, 0x1234u16, 0000, 0000, 0x1b12, 0000, 0000, 0x1a13u16,
-    ];
-    // let ipv6: [u16; 8] = [0x2u16, 0x3u16, 0, 0, 0x1bu16, 0, 0, 0x1u16];
-    let ipv6: [u16; 8] = [1, 0, 0, 0, 0, 0, 0, 1];
+    let ipv6_a = "2a03:2880:f12f:183:face:b00c:0:25de";
+    //let ipv6_a = "1:0:1:0:1:0:1:0";
+    //let ipv6_a = "fe0::1";
+    let ipv6 = ipv6_a.parse::<Ipv6Addr>().unwrap().segments();
 
     let mut in_zero_streak = true;
-    let mut zero_streak_start = 0;
     let mut zero_streak_length = 0;
-    let mut zero_group_idx: u8 = 0;
-    let mut last_zgroup: Option<&ZeroGroup> = None;
-    for (idx, entry) in ipv6.iter().enumerate() {
+    for entry in ipv6.iter() {
         if entry == &0 {
-            if !in_zero_streak {
-                zero_streak_start = idx.try_into().unwrap();
-            }
             zero_streak_length += 1;
             in_zero_streak = true;
         } else {
             if in_zero_streak {
-                if zero_streak_length > 1 {
+                if zero_streak_length >= 1 {
                     ip6addr.segments.push(IPv6Segment::ZeroSegment(ZeroGroup {
-                        start: zero_streak_start,
                         count: zero_streak_length,
                     }));
-                    ip6addr.zero_segments += 1;
                 }
+                ip6addr.segments.push(IPv6Segment::NonZeroSegment(*entry));
+            } else {
                 ip6addr.segments.push(IPv6Segment::NonZeroSegment(*entry));
             }
             in_zero_streak = false;
@@ -94,11 +98,9 @@ fn main() {
     }
     if in_zero_streak {
         ip6addr.segments.push(IPv6Segment::ZeroSegment(ZeroGroup {
-            start: zero_streak_start,
             count: zero_streak_length,
         }));
-        ip6addr.zero_segments += 1;
     }
 
-    println!("{:#?}", ip6addr);
+    println!("{:?} : {:#?}", ipv6_a, ip6addr);
 }
