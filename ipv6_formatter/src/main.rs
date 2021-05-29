b73f904d5b49614f3e8b5001556e38f9ca7f5eaf
Abhijit Gadgil <gabhijit@iitbombay.org>
2021-05-29 17:35:21 +0530

    Fixes to IPv6 formatting

--- ipv6_formatter/src/main.rs
+++ ipv6_formatter/src/main.rs
@@ -71,11 +71,11 @@ fn main() {
     let mut ip6addr: IPv6 = IPv6::default();
 
     let ipv6_a = "2a03:2880:f12f:183:face:b00c:0:25de";
-    //let ipv6_a = "1:0:1:0:1:0:1:0";
+    let ipv6_a = "1:0:1:0:1:0:1:0";
     //let ipv6_a = "fe0::1";
     let ipv6 = ipv6_a.parse::<Ipv6Addr>().unwrap().segments();
 
-    let mut in_zero_streak = true;
+    let mut in_zero_streak = false;
     let mut zero_streak_length = 0;
     for entry in ipv6.iter() {
         if entry == &0 {
@@ -83,15 +83,12 @@ fn main() {
             in_zero_streak = true;
         } else {
             if in_zero_streak {
-                if zero_streak_length >= 1 {
-                    ip6addr.segments.push(IPv6Segment::ZeroSegment(ZeroGroup {
-                        count: zero_streak_length,
-                    }));
-                }
-                ip6addr.segments.push(IPv6Segment::NonZeroSegment(*entry));
-            } else {
-                ip6addr.segments.push(IPv6Segment::NonZeroSegment(*entry));
+                ip6addr.segments.push(IPv6Segment::ZeroSegment(ZeroGroup {
+                    count: zero_streak_length,
+                }));
             }
+            ip6addr.segments.push(IPv6Segment::NonZeroSegment(*entry));
+
             in_zero_streak = false;
             zero_streak_length = 0;
         }
